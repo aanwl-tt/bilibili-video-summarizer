86ea36a32f84833a30efa19644532b2814c4e67c
aanwl-tt <aanwl-tt@users.noreply.github.com>
2026-05-12 20:05:03 +0800

    Remove unused shared barrel export and url utils

--- extension/src/shared/index.ts
+++
@@ -1,6 +0,0 @@
-export * from "./types/video";
-export * from "./types/api";
-export * from "./types/settings";
-export * from "./constants/bilibili";
-export * from "./constants/providers";
-export * from "./utils/url";
